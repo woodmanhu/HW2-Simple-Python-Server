ea5b816f1575101352397513b9273a9ad7b081d1
woodmanhu <woodmanhu@gmail.com>
2015-02-18 10:23:05 -0600

    QIwen Hu Homework1 Version 1.0

--- HTTPServer.py
+++ HTTPServer.py
@@ -1,3 +1,4 @@
+
 #University of St. Thomas, Advanced Web Applications, SEIS752 Fall 2009
 #Lloyd Cledwyn
 #Basic Http Server example
@@ -8,7 +9,7 @@ from os import curdir, sep
 
 #Define some variables to be used in the execution of the program
 HOST_NAME = '' # can be 'localhost' or if you change your hosts.txt file, what happens?? ;)
-PORT_NUMBER = 9001 #If you kill the server un-gracefully you may need to change this to an open socket.
+PORT_NUMBER = 9000 #If you kill the server un-gracefully you may need to change this to an open socket.
 
 class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 #MyHandler class implements standard standard HTTP menthods
@@ -18,7 +19,7 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_header("Content-type", "text/html")
         self.end_headers()
     def do_GET(self):
-        #Respond to a GET request.
+       
         try:
             if self.path.endswith((".html",".htm")):
                 f = open(curdir + sep + self.path) #self.path has /index.html
@@ -39,11 +40,23 @@ class MyHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                 return
                 
             if self.path.endswith(".gif"):
-                # IMPLEMENT THIS
+                f = open(curdir + sep + self.path, 'rb') #self.path has /index.html
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                # Start sending content
+                self.wfile.write(f.read())
+                f.close()
                 return
                 
             if self.path.endswith(".jpg"):
-                # IMPLEMENT THIS
+                f = open(curdir + sep + self.path, 'rb') #self.path has /index.html
+                self.send_response(200)
+                self.send_header("Content-type", "text/html")
+                self.end_headers()
+                # Start sending content
+                self.wfile.write(f.read())
+                f.close()
                 return
         except IOError:
             self.send_error(404,'File not found: %self' % self.path)
